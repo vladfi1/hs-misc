b3090b950ebfcd28cd55e075a055885afdf9a21f
Vlad Firoiu <vladfi2@gmail.com>
2015-10-19 18:21:02 -0400

    Move completeness check to JavaGeneric.

--- JavaGeneric.hs
+++ JavaGeneric.hs
@@ -6,17 +6,13 @@
 module JavaGeneric where
 
 import Language.Java.Syntax
-import Language.Java.Parser
 
-import qualified GHC.Generics as GHC
 import Generics.SOP
 import Generics.SOP.TH
 
 import Data.Constraint
---import JavaTH
-import TypeLevel
 import List
---  import Language.Haskell.TH
+import TypeLevel
 
 -- use a type family to break the cyclic class
 -- also to do case analysis on the type
@@ -29,11 +25,6 @@ type family AllGeneric' x :: Constraint where
 
 class AllGeneric' x => AllGeneric x
 instance AllGeneric' x => AllGeneric x
-
---deriving instance GHC.Generic CompilationUnit
---instance Generic CompilationUnit
-
---concat <$> (mapM deriveGeneric =<< javaSyntax)
 
 concat <$> traverse deriveGeneric
   [''CompilationUnit
@@ -87,4 +78,107 @@ concat <$> traverse deriveGeneric
 
 allGeneric :: Dict (AllGeneric CompilationUnit)
 allGeneric = Dict
+
+type PrimTypes = [Char, Int, Integer, Double]
+
+type GenericTypes =
+  [ CompilationUnit
+  , TypeDecl
+  , PackageDecl
+  , Name
+  , InterfaceDecl
+  , TypeParam
+  , RefType
+  , Type
+  , PrimType
+  , Modifier
+  , InterfaceBody
+  , MemberDecl
+  , VarDecl
+  , VarInit
+  , VarDeclId
+  , MethodBody
+  , ImportDecl
+  , Ident
+  , FormalParam
+  , Exp
+  , TypeArgument
+  , WildcardBound
+  , Op
+  , MethodInvocation
+  , Literal
+  , Lhs
+  , FieldAccess
+  , ConstructorBody
+  , ExplConstrInv
+  , ClassType
+  , ClassDecl
+  , ClassBody
+  , Annotation
+  , ArrayInit
+  , ArrayIndex
+  , AssignOp
+  , Block
+  , BlockStmt
+  , Stmt
+  , SwitchBlock
+  , SwitchLabel
+  , Decl
+  , ElementValue
+  , EnumBody
+  , EnumConstant
+  , ForInit
+  , Catch
+  , Bool
+  , [Char]
+  , [(Ident, [TypeArgument])]
+  , (Ident, [TypeArgument])
+  , [TypeArgument]
+  , [(Ident, ElementValue)]
+  , (Ident, ElementValue)
+  , [VarInit]
+  , [VarDecl]
+  , [TypeParam]
+  , [TypeDecl]
+  , [SwitchBlock]
+  , [RefType]
+  , [Modifier]
+  , [MemberDecl]
+  , [ImportDecl]
+  , [Ident]
+  , [FormalParam]
+  , [Argument]
+  , [EnumConstant]
+  , [Decl]
+  , [Catch]
+  , [BlockStmt]
+  , [Exp]
+  , Maybe [Exp]
+  , Maybe Exp
+  , Maybe WildcardBound
+  , Maybe VarInit
+  , Maybe Type
+  , Maybe RefType
+  , Maybe PackageDecl
+  , Maybe Ident
+  , Maybe ForInit
+  , Maybe ExplConstrInv
+  , Maybe ClassBody
+  , Maybe Block
+  ]
+
+
+type AllTypes = PrimTypes :++: GenericTypes
+
+type family Codes xs where
+  Codes '[] = '[]
+  Codes (x ': xs) = Code x ': Codes xs
+
+type family All3 (c :: k -> Constraint) (ksss :: [[[k]]]) :: Constraint where
+  All3 c '[] = ()
+  All3 c (kss ': ksss) = (All2 c kss, All3 c ksss)
+
+--complete :: Dict (All2 (Find AllTypes) (TypeLevel.Concat (Codes GenericTypes)))
+complete :: Dict (All3 (Find AllTypes) (Codes GenericTypes))
+complete = Dict
 
