3e039df7136aaff9444720412cacce1deb84269c
Vlad Firoiu <vladfi2@gmail.com>
2015-10-19 18:18:06 -0400

    Rename Nil to VNil and Cons to VCons. Vec append.

--- Vec.hs
+++ Vec.hs
@@ -1,45 +1,50 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Vec where
 
-import Prelude hiding (replicate, reverse)
+import Prelude hiding (replicate, reverse, concat)
 import Zippable
 import Nats
 
 data Vec n a where
-  Nil :: Vec Z a
-  Cons :: a -> Vec n a -> Vec (S n) a
+  VNil :: Vec Z a
+  VCons :: a -> Vec n a -> Vec (S n) a
 
 index :: LT n -> Vec n a -> a
-index ZLT (Cons a _) = a
-index (SLT n) (Cons _ l) = index n l
+index ZLT (VCons a _) = a
+index (SLT n) (VCons _ l) = index n l
 
 instance Functor (Vec n) where
-  fmap f Nil = Nil
-  fmap f (Cons a l) = Cons (f a) (fmap f l)
+  fmap f VNil = VNil
+  fmap f (VCons a l) = VCons (f a) (fmap f l)
 
 instance Foldable (Vec n) where
-  foldr f b Nil = b
-  foldr f b (Cons a l) = f a (foldr f b l)
+  foldr f b VNil = b
+  foldr f b (VCons a l) = f a (foldr f b l)
   
-  foldl f b Nil = b
-  foldl f b (Cons a l) = foldl f (f b a) l
+  foldl f b VNil = b
+  foldl f b (VCons a l) = foldl f (f b a) l
 
 instance Traversable (Vec n) where
-  sequenceA Nil = pure Nil
-  sequenceA (Cons a l) = Cons <$> a <*> sequenceA l
+  sequenceA VNil = pure VNil
+  sequenceA (VCons a l) = VCons <$> a <*> sequenceA l
 
 replicate :: SNat n -> a -> Vec n a
-replicate SZ _ = Nil
-replicate (SS n) a = Cons a (replicate n a)
+replicate SZ _ = VNil
+replicate (SS n) a = VCons a (replicate n a)
 
 instance Zippable (Vec n) where
-  Nil <**> Nil = Nil
-  Cons f fs <**> Cons a as = Cons (f a) (fs <**> as)
+  VNil <**> VNil = VNil
+  VCons f fs <**> VCons a as = VCons (f a) (fs <**> as)
 
 -- ZipVec semantics
 instance (ReifyNat n) => Applicative (Vec n) where
   pure = replicate nat
   (<*>) = (<**>)
+
+appendVec :: Vec n a -> Vec m a -> Vec (n :+: m) a
+appendVec VNil v = v
+appendVec (VCons a v1) v2 = VCons a (appendVec v1 v2)
 
