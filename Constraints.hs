5ebfbc79577e0490a565aaed2d9a6ce96146de61
Vlad Firoiu <vladfi2@gmail.com>
2015-10-28 03:20:35 -0400

    Some useful constraint formers.

--- Constraints.hs
+++ Constraints.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE PolyKinds, ConstraintKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Constraints where
 
@@ -20,9 +23,31 @@ transitivity ab bc = Sub $ sub ab $ sub bc Dict
 
 newtype Forall c = Forall (forall a. Dict (c a))
 
+class ForallC c where
+  forallC :: Forall c
+
+forallImplies :: Implies (ForallC c) (c a)
+forallImplies = Sub $
+  case forallC of
+    Forall dict -> dict
+
+--withForall :: forall c b r. (forall a. (Dict (c a))) -> (c b => r) -> r
+--withForall (Dict :: Dict (c b)) r = r
+
+withForall :: forall c a r. Forall c -> (c a => r) -> r
+withForall (Forall (Dict :: Dict (c a))) r = r
+
 class Trivial a
 instance Trivial a
 
 all :: Forall Trivial
 all = Forall Dict
+
+class c b a => FlipC c a b
+instance c b a => FlipC c a b
+
+class c (f a) => CompC c f a
+instance c (f a) => CompC c f a
+
+type ForallC1 c f = ForallC (CompC c f)
 
