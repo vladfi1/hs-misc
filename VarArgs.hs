f1edebacaa6ec6a212cdaecf423e49cd45dd8c74
Vlad Firoiu <vladfi2@gmail.com>
2015-11-12 18:51:42 -0500

    Switch to MPTC and FunDeps for better type inference.

--- VarArgs.hs
+++ VarArgs.hs
@@ -1,25 +1,33 @@
 {-# LANGUAGE TypeFamilies, TypeOperators #-}
 {-# LANGUAGE DataKinds, PolyKinds #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
 
 module VarArgs where
 
 import Data.Vinyl
+import Data.Vinyl.Functor
 
-type family Curried (f :: k -> *) (l :: [k]) (r :: *) :: * where
-  Curried f '[] r = r
+import Prelude hiding (curry, uncurry)
+
+type family Curried (f :: k -> *) (l :: [k]) (r :: k) :: * where
+  Curried f '[] r = Identity r
   Curried f (a ': l) r = f a -> Curried f l r
 
-class Curry (l :: [k]) where
-  curry' :: (Rec f l -> a) -> Curried f l a
+-- use singletons instead of a custom class?
+class Curry f l r c | f l r -> c, f c -> l, f c -> r where
+  curry' :: (Rec f l -> Identity r) -> c
+  uncurry' :: c -> Rec f l -> Identity r
 
-instance Curry '[] where
+curry f = curry' (Identity . f)
+uncurry f = getIdentity . (uncurry' f)
+
+instance Curry f '[] r (Identity r) where
   curry' f = f RNil
+  uncurry' f RNil = f
 
-instance Curry l => Curry (a ': l) where
+instance Curry f l r c => Curry f (a ': l) r (f a -> c) where
   curry' f fa = curry' (\args -> f $ fa :& args)
+  uncurry' f (fa :& args) = uncurry' (f fa) args
 
-{-
-type family Uncurry (f :: k -> *) (g :: *) :: * where
-  Uncurry (f a -> b) = 
-  Uncurry r = 
--}
+
