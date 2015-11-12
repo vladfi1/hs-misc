37c92fbdc611045dc8d6c806783a0e8df19028b4
Vlad Firoiu <vladfi2@gmail.com>
2015-11-12 14:32:08 -0500

    Use existential instead of GADT.

--- DAGIO.hs
+++ DAGIO.hs
@@ -14,13 +14,10 @@ import Data.Vinyl.Functor
 import Control.Monad
 import Data.Functor.Sum
 
-data SomeNode f output where
-  SomeNode :: Node f inputs output -> SomeNode f output
+type NodeOrRef f = Sum (Node f) (IORef :. f)
 
-type NodeOrRef f = Sum (SomeNode f) (IORef :. f)
-
-data Node f inputs output
-  = Node
+data Node f output
+  = forall inputs. Node
   { forward :: Rec f inputs -> f output
   , inputs :: Rec (NodeOrRef f) inputs
   , output :: IORef (f output)
@@ -34,10 +31,10 @@ rtraverse_ f (x :& xs) = f x *> rtraverse_ f xs
 rtraverse_ _ RNil = pure ()
 
 resetNodeOrRef :: NodeOrRef f a -> IO ()
-resetNodeOrRef (InL (SomeNode node)) = resetNode node
+resetNodeOrRef (InL node) = resetNode node
 resetNodeOrRef _ = return ()
 
-resetNode :: Node f inputs output -> IO ()
+resetNode :: Node f output -> IO ()
 resetNode Node{inputs, updated} = do
   todo <- readIORef updated
   when todo $ do
@@ -45,10 +42,10 @@ resetNode Node{inputs, updated} = do
     writeIORef updated False
 
 evalNodeOrRef :: NodeOrRef f output -> IO (f output)
-evalNodeOrRef (InL (SomeNode node)) = evalNode node
+evalNodeOrRef (InL node) = evalNode node
 evalNodeOrRef (InR (Compose ref)) = readIORef ref
 
-evalNode :: Node f inputs output -> IO (f output)
+evalNode :: Node f output -> IO (f output)
 evalNode Node{..} = do
   done <- readIORef updated
   unless done $ do
