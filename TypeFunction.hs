80ff59c799b27fef5085046327cef75252401e94
Vlad Firoiu <vladfi2@gmail.com>
2015-11-14 02:20:54 -0500

    Assert that MPTCs + FunDeps don't give open, overlappable type functions.

--- TypeFunction.hs
+++ TypeFunction.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
 
 module TypeFunction where
 
 type family F x where
   F Int = Int
   F x = Bool
+
+class C x r | x -> r
+
+instance C Int Int
+instance {-# OVERLAPPABLE #-} C x Bool
 
