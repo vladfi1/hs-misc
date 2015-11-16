859bfe0ebbdbc7db0862944c16885df7469982d8
Vlad Firoiu <vladfi2@gmail.com>
2015-11-15 20:28:15 -0500

    Testing to see if FunDeps work as functions. They do not.

--- TypeFunction.hs
+++ TypeFunction.hs
@@ -1,14 +1,24 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
 
 module TypeFunction where
+
+import Data.Type.Equality
 
 type family F x where
   F Int = Int
   F x = Bool
 
+-- candidate type-level "function"
 class C x r | x -> r
 
+{- Fails FunDep checker.
 instance C Int Int
 instance {-# OVERLAPPABLE #-} C x Bool
+-}
+
+{- GHC can't invert FunDep :(
+invert :: (C x r, C y s) => x :~: y -> r :~: s
+invert Refl = Refl
+-}
 
