6e0ae950dff1795c37e2a5817e26c18032dd2293
Vlad Firoiu <vladfi2@gmail.com>
2015-11-19 16:10:23 -0500

    Some Fractional instances and utility functions.

--- TensorHMatrix.hs
+++ TensorHMatrix.hs
@@ -12,6 +12,7 @@ module TensorHMatrix where
 import Data.Proxy
 import GHC.TypeLits hiding (natVal')
 import Numeric.LinearAlgebra
+import Data.Vector.Storable
 import Data.Default
 
 type Usable a = (Element a, Num a, Numeric a, Num (Vector a), Container Vector a)
@@ -35,6 +36,8 @@ instance (KnownNat n, Default a, Usable a) => Default (Tensor a '[n]) where
 instance (KnownNat n, KnownNat m, Default a, Usable a) => Default (Tensor a '[n, m]) where
   def = Matrix $ konst def (natVal' (Proxy::Proxy n), natVal' (Proxy::Proxy m))
 
+fill1 :: forall a n. (KnownNat n, Usable a) => a -> Tensor a '[n]
+fill1 a = Vector $ konst a (natVal' (Proxy::Proxy n))
 
 instance Num a => Num (Tensor a '[]) where
   Scalar a + Scalar b = Scalar (a + b)
@@ -63,6 +66,16 @@ instance (KnownNat n, KnownNat m, Usable a) => Num (Tensor a '[n, m]) where
   signum (Matrix a) = Matrix (signum a)
   fromInteger n = Matrix $ konst (fromInteger n) (natVal' (Proxy::Proxy n), natVal' (Proxy::Proxy m))
 
+instance Fractional a => Fractional (Tensor a '[]) where
+  Scalar a / Scalar b = Scalar (a / b)
+  recip (Scalar a) = Scalar (recip a)
+  fromRational r = Scalar (fromRational r)
+
+instance (KnownNat n, Numeric a, Fractional a, Fractional (Vector a)) => Fractional (Tensor a '[n]) where
+  Vector a / Vector b = Vector (a / b)
+  recip (Vector a) = Vector (recip a)
+  fromRational r = Vector $ konst (fromRational r) (natVal' (Proxy::Proxy n))
+
 tmap :: (Container Vector a, Num a, Element b) => (a -> b) -> Tensor a dims -> Tensor b dims
 tmap f (Scalar a) = Scalar $ f a
 tmap f (Vector v) = Vector $ cmap f v
@@ -74,3 +87,5 @@ transpose (Matrix m) = Matrix (tr m)
 mv :: Numeric a => Tensor a '[n, m] -> Tensor a '[m] -> Tensor a '[n]
 mv (Matrix m) (Vector v) = Vector $ m #> v
 
+oneHot :: forall a n. (KnownNat n, Usable a) => Int -> Tensor a '[n]
+oneHot m = Vector $ (konst 0 (natVal' (Proxy::Proxy n))) // [(m, 1)]
