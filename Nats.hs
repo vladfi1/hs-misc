09fc4ab7b7df448450022d6e90801efa6d328178
Vlad Firoiu <vladfi2@gmail.com>
2015-11-27 21:05:29 -0500

    Fully integrate with singletons.

--- Nats.hs
+++ Nats.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, PolyKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Nats where
@@ -14,12 +16,15 @@ import Data.Proxy
 import qualified GHC.TypeLits as TL
 --import Generics.SOP.Sing
 import Data.Singletons.Prelude
-import Data.Singletons.TH (genSingletons)
+import Data.Singletons.TH
+import Data.Singletons.Prelude.Enum
+import Data.Singletons.TypeLits (withKnownNat)
+import Unsafe.Coerce
 
-data Nat = Z | S Nat
-  deriving (Eq, Ord, Show)
-
-genSingletons [''Nat]
+$(singletons [d|
+  data Nat = Z | S Nat
+    deriving (Eq, Ord)
+  |])
 
 instance Enum Nat where
   succ = S
@@ -33,51 +38,84 @@ instance Enum Nat where
   fromEnum Z = 0
   fromEnum (S n) = 1 + (fromEnum n)
 
-{-
-type family Min (x :: Nat) (y :: Nat) :: Nat where
-  Min Z y = Z
-  Min x Z = Z
-  Min (S x) (S y) = S (Min x y)
+type family FromTLNat (n :: TL.Nat) :: Nat where
+  FromTLNat 0 = Z
+  FromTLNat n = S (FromTLNat (n TL.- 1))
 
-type family Max (x :: Nat) (y :: Nat) :: Nat where
-  Max Z y = y
-  Max x Z = x
-  Max (S x) (S y) = S (Max x y)
--}
+instance PEnum ('KProxy :: KProxy Nat) where
+  type Succ n = S n
+  type Pred Z = Error "Pred Z"
+  type Pred (S n) = n
+  type ToEnum n = FromTLNat n
+  type FromEnum Z = 0
+  type FromEnum (S n) = 1 TL.+ (FromEnum n)
 
-infix 4 :<:
-type family (x :: Nat) :<: (y :: Nat) :: Bool where
-  x :<: Z = False
-  Z :<: (S x) = True
-  (S x) :<: (S y) = x :<: y
+instance SEnum ('KProxy :: KProxy Nat) where
+  sSucc = SS
+  sPred SZ = error "Pred SZ"
+  sPred (SS n) = n
 
-infix 4 :<=:
-type family (x :: Nat) :<=: (y :: Nat) :: Bool where
-  Z :<=: x = True
-  (S x) :<=: Z = False
-  (S x) :<=: (S y) = x :<=: y
+  sToEnum = sFromInteger
+  -- I don't think we can write sFromEnum
 
-infixl 6 :+:
-type family (x :: Nat) :+: (y :: Nat) :: Nat where
-  Z :+: x = x
-  (S x) :+: y = S (x :+: y)
+instance Num Nat where
+  Z + x = x
+  S x + y = S (x + y)
+
+  x - Z = x
+  Z - _ = error "Negative Nat."
+  S x - S y = x - y
+
+  Z * _ = Z
+  S x * y = y + (x * y)
+
+  fromInteger 0 = Z
+  fromInteger n = S (fromInteger (n-1))
+
+  negate = error "Can't negate Nats."
+  abs = id
+  signum _ = 1
+
+instance PNum ('KProxy :: KProxy Nat) where
+  type Z :+ x = x
+  type S x :+ y = S (x :+ y)
+
+  type x :- Z = x
+  type Z :- S x = Error "Negative Nat"
+  type S x :- S y = x :- y
+
+  type Z :* x = Z
+  type S x :* y = y :+ (x :* y)
+
+  type Abs (n :: Nat) = n
+  type Signum n = S Z
+
+  type FromInteger (n :: TL.Nat) = FromTLNat n
+
+instance SNum ('KProxy :: KProxy Nat) where
+  SZ %:+ x = x
+  SS x %:+ y = SS (x %:+ y)
+
+  x %:- SZ = x
+  SZ %:- _ = error "Negative SNat"
+  SS x %:- SS y = x %:- y
+
+  SZ %:* _ = SZ
+  SS x %:* y = y %:+ (x %:* y)
+
+  sAbs = id
+  sSignum _ = SS SZ
+  sFromInteger n = withKnownNat n $
+    case TL.natVal n of
+      0 -> unsafeCoerce SZ
+      _ -> unsafeCoerce $ SS (sFromInteger $ sPred n)
+
 
 type family Sum (xs :: [Nat]) :: Nat where
   Sum '[] = Z
-  Sum (x ': xs) = x :+: (Sum xs)
+  Sum (x ': xs) = x :+ (Sum xs)
 
-infixl 6 :-:
-type family (x :: Nat) :-: (y :: Nat) :: Nat where
-  Z :-: x = Z
-  x :-: Z = x
-  (S x) :-: (S y) = x :-: y
-
--- this requires undecidable instances...
-infixl 7 :*:
-type family (x :: Nat) :*: (y :: Nat) :: Nat where
-  Z :*: x = Z
-  (S x) :*: y = y :+: (x :*: y)
-
+{-
 infixl 7 :/:
 type family (x :: Nat) :/: (y :: Nat) :: Nat where
   Z :/: y = Z -- avoid infinite loops
@@ -101,50 +139,23 @@ type family GCD (m :: Nat) (n :: Nat) :: Nat where
 
 gcd' :: p1 m -> p2 n -> Proxy (GCD m n)
 gcd' _ _ = Proxy
-
-type family ToNat (n :: TL.Nat) :: Nat where
-  ToNat 0 = Z
-  ToNat n = S (ToNat (n TL.- 1))
-
-{-
-type SNat = (Sing :: Nat -> *)
-
-data instance Sing (n :: Nat) where
-  SZ :: SNat Z
-  SS :: SNat n -> SNat (S n)
-
-instance SingI Z where
-  sing = SZ
-
-instance (SingI n) => SingI (S n) where
-  sing = SS sing
 -}
 
-snat2nat :: SNat n -> Nat
-snat2nat SZ = Z
-snat2nat (SS n) = S (snat2nat n)
+instance Show Nat where
+  show = show . fromEnum
 
 instance Show (SNat n) where
-  show = show . fromEnum . snat2nat
+  show = show . fromSing
 
-snat :: (n' ~ ToNat n, SingI n') => proxy n -> SNat n'
-snat _ = sing
-
-zero = SZ
-one = SS zero
-two = SS one
-three = SS two
-
-nPred :: proxy (S n) -> Proxy n
-nPred _ = Proxy
-
-nSucc :: proxy n -> Proxy (S n)
-nSucc _ = Proxy
+s0 = SZ
+s1 = SS s0
+s2 = SS s1
+s3 = SS s2
 
 data LTE n where
-  ZLTE :: LTE n
-  SLTE :: LTE n -> LTE (S n)
+  LTE_Z :: LTE n
+  LTE_S :: LTE n -> LTE (S n)
 
 data LT n where
-  ZLT :: LT (S n)
-  SLT :: LT n -> LT (S n)
+  LT_Z :: LT (S n)
+  LT_S :: LT n -> LT (S n)
