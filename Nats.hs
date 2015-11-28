9abb8c1c6b94957e910bac48b20249be9fabd459
Vlad Firoiu <vladfi2@gmail.com>
2015-11-28 17:23:14 -0500

    Let singletons handle Enum and Nat promotion.

--- Nats.hs
+++ Nats.hs
@@ -11,104 +11,45 @@
 
 module Nats where
 
---import TypeLevel
-import Data.Proxy
-import qualified GHC.TypeLits as TL
---import Generics.SOP.Sing
 import Data.Singletons.Prelude
 import Data.Singletons.TH
 import Data.Singletons.Prelude.Enum
-import Data.Singletons.TypeLits (withKnownNat)
-import Unsafe.Coerce
 
 $(singletons [d|
   data Nat = Z | S Nat
     deriving (Eq, Ord)
+
+  instance Enum Nat where
+    succ n = S n
+
+    pred Z = error "pred Z"
+    pred (S n) = n
+
+    toEnum n = if n == 0 then Z else S $ toEnum (n-1)
+
+    fromEnum Z = 0
+    fromEnum (S n) = 1 + (fromEnum n)
+
+  instance Num Nat where
+    Z + x = x
+    S x + y = S (x + y)
+
+    x - Z = x
+    Z - S _ = error "Z - S _"
+    S x - S y = x - y
+
+    Z * _ = Z
+    S x * y = y + (x * y)
+
+    fromInteger n = if n == 0 then Z else S (fromInteger (n-1))
+
+    negate Z = Z
+    negate (S _) = error "negate (S _)"
+    
+    abs n = n
+    signum _ = 1
+
   |])
-
-instance Enum Nat where
-  succ = S
-
-  pred Z = Z
-  pred (S n) = n
-
-  toEnum 0 = Z
-  toEnum n = S $ toEnum (n-1)
-
-  fromEnum Z = 0
-  fromEnum (S n) = 1 + (fromEnum n)
-
-type family FromTLNat (n :: TL.Nat) :: Nat where
-  FromTLNat 0 = Z
-  FromTLNat n = S (FromTLNat (n TL.- 1))
-
-instance PEnum ('KProxy :: KProxy Nat) where
-  type Succ n = S n
-  type Pred Z = Error "Pred Z"
-  type Pred (S n) = n
-  type ToEnum n = FromTLNat n
-  type FromEnum Z = 0
-  type FromEnum (S n) = 1 TL.+ (FromEnum n)
-
-instance SEnum ('KProxy :: KProxy Nat) where
-  sSucc = SS
-  sPred SZ = error "Pred SZ"
-  sPred (SS n) = n
-
-  sToEnum = sFromInteger
-  -- I don't think we can write sFromEnum
-
-instance Num Nat where
-  Z + x = x
-  S x + y = S (x + y)
-
-  x - Z = x
-  Z - _ = error "Negative Nat."
-  S x - S y = x - y
-
-  Z * _ = Z
-  S x * y = y + (x * y)
-
-  fromInteger 0 = Z
-  fromInteger n = S (fromInteger (n-1))
-
-  negate = error "Can't negate Nats."
-  abs = id
-  signum _ = 1
-
-instance PNum ('KProxy :: KProxy Nat) where
-  type Z :+ x = x
-  type S x :+ y = S (x :+ y)
-
-  type x :- Z = x
-  type Z :- S x = Error "Negative Nat"
-  type S x :- S y = x :- y
-
-  type Z :* x = Z
-  type S x :* y = y :+ (x :* y)
-
-  type Abs (n :: Nat) = n
-  type Signum n = S Z
-
-  type FromInteger (n :: TL.Nat) = FromTLNat n
-
-instance SNum ('KProxy :: KProxy Nat) where
-  SZ %:+ x = x
-  SS x %:+ y = SS (x %:+ y)
-
-  x %:- SZ = x
-  SZ %:- _ = error "Negative SNat"
-  SS x %:- SS y = x %:- y
-
-  SZ %:* _ = SZ
-  SS x %:* y = y %:+ (x %:* y)
-
-  sAbs = id
-  sSignum _ = SS SZ
-  sFromInteger n = withKnownNat n $
-    case TL.natVal n of
-      0 -> unsafeCoerce SZ
-      _ -> unsafeCoerce $ SS (sFromInteger $ sPred n)
 
 instance Real Nat where
   toRational = toRational . fromEnum
