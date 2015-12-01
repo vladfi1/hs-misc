dc811d6abb5e2f9aed9b8b45c78bf69dfe9a00ba
Vlad Firoiu <vladfi2@gmail.com>
2015-12-01 13:14:58 -0500

    Attempt to differentiate eval.

--- DSL.hs
+++ DSL.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+
 module DSL where
 
 import Numeric.AD
@@ -7,11 +9,18 @@ data Exp a
   | Neg (Exp a)
   | (Exp a) :+: (Exp a)
   | (Exp a) :*: (Exp a)
+  deriving (Functor, Foldable, Traversable)
 
 instance Num a => Num (Exp a) where
   (+) = (:+:)
   (*) = (:*:)
   fromInteger = Const . fromInteger
   negate = Neg
+
+eval :: Num a => Exp a -> a
+eval (Const a) = a
+eval (Neg e) = negate (eval e)
+eval (e1 :+: e2) = eval e1 + eval e2
+eval (e1 :*: e2) = eval e1 * eval e2
 
 
