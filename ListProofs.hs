56fab3023703ef5a0c659590307aba0b5dfe9def
Vlad Firoiu <vladfi2@gmail.com>
2015-12-01 13:15:22 -0500

    Renaming to avoid clash with Singletons.

--- ListProofs.hs
+++ ListProofs.hs
@@ -1,32 +1,40 @@
+{-# LANGUAGE GADTs, DataKinds, TypeFamilies, TypeOperators, PolyKinds #-}
+
 module ListProofs where
 
-data Map f l r where
-  MapNil :: Map f '[] '[]
-  MapCons :: Map f as bs -> Map f (a ': as) (f a ': bs)
+import Data.Singletons.Prelude
 
-data FoldR f b l r where
-  FoldRNil :: FoldR f b '[] b
-  FoldRCons :: FoldR f b l r -> FoldR f b (a ': l) (f a r)
+data MapP f l r where
+  MapNil :: MapP f '[] '[]
+  MapCons :: MapP f as bs -> MapP f (a ': as) (Apply f a ': bs)
 
-type Concat s l = FoldR '(:) l s
+mapP :: SList l -> MapP f l (Map f l)
+mapP SNil = MapNil
+mapP (SCons _ l) = MapCons (mapP l)
 
-concatNil :: SList l -> Concat l '[] l
-concatNil SNil' = FoldRNil
-concatNil (SCons' _ l) = FoldRCons (concatNil l)
+data FoldRP f b l r where
+  FoldRNil :: FoldRP f b '[] b
+  FoldRCons :: FoldRP f b l r -> FoldRP f b (a ': l) (f a r)
+
+type ConcatP s l = FoldRP '(:) l s
+
+concatNil :: SList l -> ConcatP l '[] l
+concatNil SNil = FoldRNil
+concatNil (SCons _ l) = FoldRCons (concatNil l)
 
 --concatAssociative :: Concat s l sl -> Concat sl r slr -> Concat l r lr -> Concat s lr slr
 --concatAssociative FoldRNil _ _ = FoldRNil
 
-data FoldL :: (b -> a -> b) -> b -> [a] -> b -> * where
-  FoldLNil :: FoldL f b '[] b
-  FoldLCons :: FoldL f (f b a) as r -> FoldL f b (a ': as) r
+data FoldLP :: (b -> a -> b) -> b -> [a] -> b -> * where
+  FoldLNil :: FoldLP f b '[] b
+  FoldLCons :: FoldLP f (f b a) as r -> FoldLP f b (a ': as) r
 
 -- a zipper with no focus
-data Zipper s l r where
-  ZipperNil :: Zipper r '[] r
-  ZipperCons :: Zipper (a ': s) l r -> Zipper s (a ': l) r
+data ZipperP s l r where
+  ZipperNil :: ZipperP r '[] r
+  ZipperCons :: ZipperP (a ': s) l r -> ZipperP s (a ': l) r
 
-type Reverse = Zipper '[]
+type ReverseP = ZipperP '[]
 
 {-
 reverseReflexive :: Zippererse l r -> Reverse r l
