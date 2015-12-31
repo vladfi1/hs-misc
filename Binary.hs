e91203947398fbbee60943f24b8ea9222ffa4b74
Vlad Firoiu <vladfi2@gmail.com>
2015-12-31 14:40:05 -0500

    Work in progress.

--- Binary.hs
+++ Binary.hs
@@ -16,20 +16,19 @@ $(singletons [d|
   sumBit a b c = a `xor` b `xor` c
   carryBit a b c = (a && b) || ((a || b) && c)
 
-  add :: [Bool] -> [Bool] -> [Bool]
-  add as [] = as
-  add [] bs = bs
-  add (a:as) (b:bs) = r : s : cs
-    where c:cs = add as bs
-          s = sumBit a b c
+  add :: Bool -> BiNat -> BiNat -> BiNat
+  add c as [] = as
+  add c [] bs = bs
+  add c (a:as) (b:bs) = s : add r as bs
+    where s = sumBit a b c
           r = carryBit a b c
   
   instance Num BiNat where
-    a + b = add a b
+    a + b = add False a b
     a - b = error "unimplemented"
     a * b = error "unimplemented"
     
-    fromInteger n = if n == 0 then [] else 1 + n
+    fromInteger n = if n == 0 then [] else 1 + (n-1)
     abs a = a
     negate a = error "negate BiNat"
   
