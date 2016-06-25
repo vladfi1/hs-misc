8df12a1597661cc069fe8f4e13d210d3c21379bd
Vlad Firoiu <vladfi2@gmail.com>
2016-06-25 15:13:41 -0400

    PHOAS-based DAG

--- DAGPHOAS.hs
+++ DAGPHOAS.hs
@@ -4,10 +4,12 @@ module DAG where
 
 import PHOAS hiding (var, let_)
 
-import GHC.Err (undefined)
-import Data.Functor
+import GHC.Err (error)
 
-import Prelude (($), show, (.), (++), const, map, fromInteger)
+import Data.Functor
+import Data.Profunctor
+
+import Prelude (($), show, (.), (++), const, map, fromInteger, String)
 
 data DAGF a b
   = Node [b]
@@ -17,12 +19,17 @@ instance Functor (DAGF a) where
   fmap f (Node bs) = Node $ map f bs
   fmap f (Let b ab) = Let (f b) (f . ab)
 
+instance Profunctor DAGF where
+  dimap a2b c2d (Node cs) = Node $ map c2d cs
+  dimap a2b c2d (Let c b2c) = Let (c2d c) (c2d . b2c . a2b)
+
+showDAGF :: DAGF (b -> String) ([String] -> String) -> [String] -> String
+showDAGF (Let b ab) (v:vs) = b vs ++ "\n" ++ "Node " ++ v ++ ": " ++ ab (const v) vs
+showDAGF (Node bs) (v:vs) = show $ map ($ vs) bs
+
 type DAG = Rec DAGF
 
-showDAGF (Node bs) vs = show $ map ($ vs) bs
-showDAGF (Let b ab) (v:vs) =
-  b vs ++ "\n" ++ "Node " ++ v ++ ": " ++ ab (const v) vs
-
+showDAG :: DAG (b -> String) ([String] -> String) -> String
 showDAG dag = cata showDAGF dag $ map show [1..]
 
 {-
@@ -37,7 +44,7 @@ var = Place
 let_ a f = Roll $ Let a f
 (>>=) = let_
 node = Roll . Node . map var
-fail = undefined
+fail = error "failure is not an option"
 
 empty = node []
 
